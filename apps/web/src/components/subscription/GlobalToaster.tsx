d8e46357fa48b70b774d3807f6561a2e3c4c4833
DOMINATOR\Fran1 <fran11799@outlook.com>
2026-05-20 18:53:25 +0200

    web: raise global sileo toaster offset above fixed header

--- apps/web/src/components/subscription/GlobalToaster.tsx
+++ apps/web/src/components/subscription/GlobalToaster.tsx
@@ -19,5 +19,5 @@ export function GlobalToaster() {
     [isDark]
   );
 
-  return <Toaster position="top-right" options={options} />;
+  return <Toaster position="top-right" offset={{ top: 96, right: 16 }} options={options} />;
 }
